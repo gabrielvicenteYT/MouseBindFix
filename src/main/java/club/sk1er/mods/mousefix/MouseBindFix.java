e679ccd4aaeeb4caee2a73aa6c9f8a045e4c3c60
asbyth <cyronize@gmail.com>
2020-02-23 23:10:37 -0500

    add modcore

--- src/main/java/club/sk1er/mods/mousefix/MouseBindFix.java
+++ src/main/java/club/sk1er/mods/mousefix/MouseBindFix.java
@@ -1,7 +1,15 @@
 package club.sk1er.mods.mousefix;
 
+import club.sk1er.modcore.ModCoreInstaller;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "mousebindfix", name = "Mouse Bind Fix", version = "1.2.1")
+@Mod(modid = "mousebindfix", name = "Mouse Bind Fix", version = "1.3")
 public class MouseBindFix {
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        ModCoreInstaller.initializeModCore(Minecraft.getMinecraft().mcDataDir);
+    }
 }
